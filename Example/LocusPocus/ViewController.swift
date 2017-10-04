e4ea7556655473cd7a944eefe14133ffdfc70b9f
Ivan Sobolevskiy <ivan.sobolevskiy@cactussoft.biz>
2017-10-04 23:04:41 +0300

    Added localized string extension

--- Example/LocusPocus/ViewController.swift
+++ Example/LocusPocus/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import LocusPocus
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let x = "y".localized()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ LocusPocus/Classes/LocalizedString.swift
@@ -0,0 +1,37 @@
+//
+//  LocalizedString.swift
+//  LocalizedString
+//
+//  Created by Ivan Sobolevskiy on 10/2/17.
+//  Copyright © 2017 Ivan Sobolevskiy. All rights reserved.
+//
+
+import Foundation
+
+/// Falls back first to the Base.lproj, then to the provided `value`.
+
+public extension String {
+    func localized(tableName: String? = nil, bundle: Bundle = Bundle.main, value: String = "", comment: String? = nil) -> String {
+        let key = self
+        return localize(with: key, tableName: tableName, bundle: bundle, value: value, comment: comment)
+    }
+    
+    mutating func localize(tableName: String? = nil, bundle: Bundle = Bundle.main, value: String = "", comment: String? = nil) {
+        let key = self
+        self = localize(with: key, tableName: tableName, bundle: bundle, value: value, comment: comment)
+    }
+    
+    private func localize(with key: String, tableName: String?, bundle: Bundle, value: String, comment: String?) -> String {
+        assert(!key.isEmpty, "Empty `key` is meaningless.")
+        var translated: String? = bundle.localizedString(forKey: key, value: value, table: tableName)
+        
+        if translated == key || translated == value {
+            if let baseLprojPath = bundle.path(forResource: "Base", ofType: "lproj") {
+                translated = Bundle(path: baseLprojPath)?.localizedString(forKey: key, value: value, table: tableName)
+            }
+        }
+        
+        guard translated != nil && translated! != key else { return key }
+        return translated!
+    }
+}
